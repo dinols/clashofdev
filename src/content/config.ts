bb95171b247e18305fe9935a64afc4942f8143a0
Lorenzo SACCO <contact@lrnz.dev>
2024-09-19 22:00:59 +0200

    chore: added content

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { z, defineCollection } from 'astro:content';
+
+const sectionCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    color: z.enum([
+      'blue',
+      'green',
+      'mustard',
+      'orange',
+      'pink',
+      'purple',
+    ] as const),
+    image: z.string().optional(),
+    icon: z.string().optional(),
+    category: z.string(),
+    points: z.string(),
+    jury: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  section: sectionCollection,
+};
